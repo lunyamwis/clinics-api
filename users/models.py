c51fc8bf328ac35344d2ecad4299fb3495fca917
lutherm <martin.luther@businesspartner.roche.com>
2023-05-17 11:32:19 +0300

    added ability to assign a role

--- users/models.py
+++ users/models.py
@@ -1,9 +1,15 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.contrib.auth.models import PermissionsMixin
+from roles.models import Role
 
 
-class CustomUser(AbstractUser):
+
+class CustomUser(AbstractUser,PermissionsMixin):
+
     name = models.CharField(blank=True, max_length=255)
+    role = models.ForeignKey(Role, on_delete=models.CASCADE, 
+                             null=True,blank=True)
 
     def __str__(self):
         return self.email
